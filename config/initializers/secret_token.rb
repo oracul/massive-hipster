4053f1dfee257a3caea889146279b6d5089010ab
oracul <daniel.holder@gmail.com>
2013-11-10 15:57:48 -0600

    food

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Food::Application.config.secret_key_base = '399e3a36b55f07f7b78e5bcb83112c4b2a05976defeb6d3bd010b1fc916451dd8f4874ef257f3e1e83fba8cfe263d20135ac0561a1e0531081c699ae9c2c9d0b'
