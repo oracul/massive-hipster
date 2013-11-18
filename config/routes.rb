71ecddbd62bc3ce02cca804f3fe65e5bfe396a3a
oracul <daniel.holder@gmail.com>
2013-11-17 21:22:21 -0600

    newer

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Food::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
- root :to =>'login'
+ root :to =>'students#index'
  get 'say/goodbye' => 'say#goodbye'
 
 
