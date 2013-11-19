fc36d7eae95d427ac1ee9fe5ea088ad32070952d
oracul <daniel.holder@gmail.com>
2013-11-18 20:56:55 -0600

    .

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
 
-	before_filter :authenticate_user!
+  before_filter :authenticate_user!
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-
   @interventions = Intervention.all
+
 
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,8 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: [:show, :edit, :update, :destroy]
 
+  before_filter :authenticate_user!
+
+  before_action :set_student, only: [:show, :edit, :update, :destroy]
   # GET /students
   # GET /students.json
   def index
