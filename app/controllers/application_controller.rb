147569f2c02eca0403a9b2d10b41f87aaa32dada
oracul <daniel.holder@gmail.com>
2013-12-07 17:00:21 -0600

    newer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,8 +19,4 @@ class ApplicationController < ActionController::Base
 
   @interventions = Intervention.all
 
-	rescue_from CanCan::AccessDenied do |exception|
-		redirect_to root_url, :alert => exception.message
-	end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,4 @@ class User < ActiveRecord::Base
    has_many :behaviors
    has_many :mstudents
    has_many :interventions
-   ROLES = %w[admin user author banned]
 end
